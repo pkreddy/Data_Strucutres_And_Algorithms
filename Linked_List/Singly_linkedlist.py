36e7653833c5f9993b2b1f94e928fd685bfd1f70
pkreddy <pranay_reddy@outlook.com>
2017-05-24 00:01:19 -0400

    Added insertion commands
    insertAtstart, insertAtEnd, insertAt

--- Linked_List/Singly_linkedlist.py
+++ Linked_List/Singly_linkedlist.py
@@ -6,7 +6,6 @@ class Node:
     def getData():
         return self.data
 
-
 class linkedlist:
     def __init__(self):
         self.head = None
@@ -17,6 +16,32 @@ class linkedlist:
             print(temp.data),
             temp = temp.next
 
+    def getLength(self):
+        temp = self.head
+        count = 0
+        while(temp):
+            count = count + 1
+            temp = temp.next
+        return count
+
+    def insertAtStart(self,data):
+        newNode = Node(data)
+        newNode.next = self.head
+        self.head = newNode
+
+    def insertAtEnd(self,data):
+        temp = self.head
+        while(temp.next):
+            temp = temp.next
+        temp.next = Node(data)
+
+    def insertAfter(self,prevNode,data):
+        if prevNode is None:
+            print("empty previous node")
+            return
+        newNode = Node(data)
+        newNode.next = prevNode.next
+        prevNode.next = newNode
 
 if __name__ == '__main__':
     llist = linkedlist()
@@ -26,4 +51,10 @@ if __name__ == '__main__':
 
     llist.head.next = second
     second.next = third
+    llist.insertAtStart(0)
+    llist.insertAtEnd(4)
+    llist.insertAfter(third,3.5)
     llist.printLinkedList()
+    
+    
+    print("length of linked list is ",llist.getLength())
